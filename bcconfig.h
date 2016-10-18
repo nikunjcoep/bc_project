99556eb6be0b9e1068620080847bfdaaa9dfb5b1
nikunjcoep <nikunjsoni123456@gmail.com>
2016-10-18 20:30:51 +0530

    Add files via upload

---
+++ bcconfig.h
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <limits.h>
+#define NDEBUG 1
+
+#define _zero_		bc_zero
+#define _one_		bc_one
+#define _two_		bc_two
+#define num2str		bc_num2str
+#define mul_base_digits bc_mul_base_digits
+
+#define bc_rt_warn rt_warn
+#define bc_rt_error rt_error
+#define bc_out_of_memory out_of_memory
+
+
+
