7a0292aaf53e7d01135dddfe4eba59b1b99cb490
nikunjcoep <nikunjsoni123456@gmail.com>
2016-11-02 21:51:43 +0530

    Add files via upload

---
+++ bcconfig.h
@@ -0,0 +1,36 @@
+/*
+    This file is part of bc_command written by Nikunj Soni.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License , or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; see the file COPYING.  If not, write to:
+
+      The Free Software Foundation, Inc.
+      59 Temple Place, Suite 330
+      Boston, MA 02111-1307 USA.
+*/
+
+#include <string.h>
+#include <limits.h>
+#define NDEBUG 1
+
+#define _zero_		bc_zero
+#define _one_		bc_one
+#define _two_		bc_two
+#define num2str		bc_num2str
+#define mul_base_digits bc_mul_base_digits
+
+#define bc_rt_warn rt_warn
+#define bc_rt_error rt_error
+#define bc_out_of_memory out_of_memory
+
+
+
